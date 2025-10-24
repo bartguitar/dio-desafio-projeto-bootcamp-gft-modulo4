6df46601e5a004f0000fffffbf890ed7a7527085
bart_studio <adrielbart@gmail.com>
2025-10-24 13:46:07 -0300

    Inicial

---
+++ src/main/java/me/dio/dio_desafio_projeto_bootcamp_gft_modulo4/DioDesafioProjetoBootcampGftModulo4Application.java
@@ -0,0 +1,13 @@
+package me.dio.dio_desafio_projeto_bootcamp_gft_modulo4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioDesafioProjetoBootcampGftModulo4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioDesafioProjetoBootcampGftModulo4Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/dio_desafio_projeto_bootcamp_gft_modulo4/DioDesafioProjetoBootcampGftModulo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.dio_desafio_projeto_bootcamp_gft_modulo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioDesafioProjetoBootcampGftModulo4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
